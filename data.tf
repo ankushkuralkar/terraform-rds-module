922c2f7fd5d3ebe318ab94dbf225211f22f6b796
wilshan <kwilshan@gmail.com>
2021-12-23 22:40:22 +0530

    commiting to dev

---
+++ data.tf
@@ -0,0 +1,23 @@
+data "aws_vpc" "vpc_available" {
+  filter {
+    name   = "tag:Name"
+    values = ["default_vpc"]
+  }
+}
+data "aws_subnet_ids" "available_db_subnet" {
+  vpc_id = data.aws_vpc.vpc_available.id
+  filter {
+    name   = "tag:Name"
+    values = ["az_1*"]
+  }
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+data "aws_security_group" "tcw_sg" {
+  filter {
+    name   = "tag:Name"
+    values = ["sg"]
+  }
+}
